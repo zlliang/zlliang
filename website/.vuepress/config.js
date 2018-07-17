513fa24eb46425136573d9aea6be013b292f44f1
Zilong Liang <zlliang15@fudan.edu.cn>
2018-07-17 18:14:06 +0800

    Migrate to VuePress and Netlify

---
+++ website/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Zilong Liang',
+  description: 'TODO',
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: 'TODO' }],
+    ['link', { rel: 'mask-icon', href: 'TODO', color: '#ffffff' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/bulma/0.7.1/css/bulma.min.css'}],
+    ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css'}],
+    ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/KaTeX/0.9.0/katex.min.css'}],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.proxy.ustclug.org/css?family=Roboto:400,400i,500,500i,700,700i'}]
+  ],
+}
---
+++ website/.vuepress/theme/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="content">
+        <h1 class="title">404</h1>
+        <p><router-link to="/">Get back to home.</router-link></p>
+      </div>
+    </div>
+  </section>
+</template>
