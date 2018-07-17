7e1118c4e9214758141800cca5a56da1c933328b
Zilong Liang <zlliang15@fudan.edu.cn>
2018-07-17 21:38:58 +0800

    Update navbar burger

--- website/.vuepress/theme/components/Header.vue
+++ website/.vuepress/theme/components/Header.vue
@@ -3,17 +3,17 @@
     <nav class="navbar is-white">
       <div class="container">
         <div class="navbar-brand">
-          <router-link to="/" class="navbar-item"><span id="site-brand">Z. LIANG</span></router-link>
-          <div class="navbar-burger" data-target="main-navbar-menu">
+          <router-link to="/" class="navbar-item"><span id="site-brand" v-on:click="forcedUntoggle">Z. LIANG</span></router-link>
+          <div class="navbar-burger"  v-bind:class="{ 'is-active': isActive }" data-target="main-navbar-menu" v-on:click="toggle">
             <span></span><span></span><span></span>
           </div>
         </div>
-        <div class="navbar-menu" id="main-navbar-menu">
+        <div class="navbar-menu" v-bind:class="{ 'is-active': isActive }" id="main-navbar-menu">
           <div class="navbar-end">
-            <router-link to="/blog/" class="navbar-item">Blog</router-link>
-            <router-link to="/news/" class="navbar-item">News</router-link>
-            <router-link to="/publications/" class="navbar-item">Publications</router-link>
-            <router-link to="/projects" class="navbar-item">Projects</router-link>
+            <router-link to="/blog/" class="navbar-item"><div v-on:click="forcedUntoggle">Blog</div></router-link>
+            <router-link to="/news/" class="navbar-item"><div v-on:click="forcedUntoggle">News</div></router-link>
+            <router-link to="/publications/" class="navbar-item"><div v-on:click="forcedUntoggle">Publications</div></router-link>
+            <router-link to="/projects" class="navbar-item"><div v-on:click="forcedUntoggle">Projects</div></router-link>
           </div>
         </div>
       </div>
@@ -23,27 +23,17 @@
 
 <script>
 export default {
-  mounted: function() {
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-
-          // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-
-          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-
-        });
-      });
+  data: function() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggle: function() {
+      this.isActive = !this.isActive
+    },
+    forcedUntoggle: function() {
+      this.isActive = false
     }
   }
 }
