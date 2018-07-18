ee069dc17349a5d0c737a1ad74d150c6411f4e4c
Zilong Liang <zlliang15@fudan.edu.cn>
2018-07-18 11:12:14 +0800

    Change CSS

--- website/.vuepress/config.js
+++ website/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: 'TODO' }],
     ['link', { rel: 'mask-icon', href: 'TODO', color: '#ffffff' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/bulma/0.7.1/css/bulma.min.css'}],
+    // ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/bulma/0.7.1/css/bulma.min.css'}],
     ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css'}],
     ['link', { rel: 'stylesheet', href: 'https://cdn.bootcss.com/KaTeX/0.6.0/katex.min.css'}],
     ['link', { rel: 'stylesheet', href: 'https://fonts.proxy.ustclug.org/css?family=Roboto:400,400i,500,500i,700,700i|Roboto+Mono'}],
