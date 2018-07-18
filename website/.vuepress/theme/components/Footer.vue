0c4f5483dcd154e09609ff59044cecd8f3a66c44
Zilong Liang <zlliang15@fudan.edu.cn>
2018-07-19 06:13:59 +0800

    Update footer

--- website/.vuepress/theme/components/Footer.vue
+++ website/.vuepress/theme/components/Footer.vue
@@ -2,27 +2,9 @@
   <footer class="section">
     <hr>
     <div class="container">
-      <div class="columns">
-        <div class="column has-text-left">
-          Site updated: {{ siteUpdated }}
-        </div>
-        <div class="column has-text-right is-hidden-mobile">
-          <p>
-            Made with <i class="fa fa-heart"></i> by <strong><a href="<%- url_for('/') %>" class="has-text-dark">Zilong Liang.</a></strong>
-          </p>
-        </div>
-      </div>
+      <p class="has-text-right">
+        Made with <i class="fa fa-heart"></i> by <strong><a href="<%- url_for('/') %>" class="has-text-dark">Zilong Liang.</a></strong>
+      </p>
     </div>
   </footer>
 </template>
-
-<script>
-import dayjs from 'dayjs'
-export default {
-  data: function() {
-    return {
-      siteUpdated: dayjs().format('MMM DD, YYYY')
-    }
-  }
-}
-</script>
