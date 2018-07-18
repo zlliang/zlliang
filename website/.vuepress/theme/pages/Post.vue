14d630a7da05710ad216d506b06cf5ba2c27965f
Zilong Liang <zlliang15@fudan.edu.cn>
2018-07-18 15:34:00 +0800

    Update line height for Chinese articles

--- website/.vuepress/theme/pages/Post.vue
+++ website/.vuepress/theme/pages/Post.vue
@@ -3,8 +3,15 @@
       <div class="container">
         <div class="content">
           <h1 class="title">{{ $page.title }}</h1>
-          <Content/>
+          <div class="line-spread" v-if="$page.frontmatter.chinese"><Content/></div>
+          <div v-else><Content/></div>
         </div>
       </div>
     </section>
 </template>
+
+<style scoped>
+.line-spread {
+  line-height: 2em;
+}
+</style>
