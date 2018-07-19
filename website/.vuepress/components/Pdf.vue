701139739d70d0bf5ada66fa4a8de66612b684b7
Zilong Liang <zlliang15@fudan.edu.cn>
2018-07-19 09:46:48 +0800

    Update PDF preview feature

--- website/.vuepress/components/Pdf.vue
+++ website/.vuepress/components/Pdf.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="pdf-container box">
+  <div class="pdf-container">
+    <div class="pdf-tools">
+      <a v-bind:href="file" class="pdf-download-button pdf-tools-span">Download</a>
+      <span class="pdf-tools-span has-text-light" v-html="file"></span>
+    </div>
     <canvas v-bind:id="file" class="pdf-canvas is-hidden-mobile"></canvas>
-    <p class="is-hidden-tablet">PDF Preview is not avaliable on mobile phones. Please <a v-bind:href="file">download</a> the file directly.</p>
+    <p class="is-hidden-tablet pdf-instruction">PDF Preview is not avaliable on mobile phones. Please <a v-bind:href="file">download</a> the file directly.</p>
   </div>
 </template>
 
@@ -10,10 +14,11 @@ export default {
   props: ['file'],
   mounted: function() {
     var filePath = this.file
+    var instruction = document.getElementById(filePath + 'ins')
 
-    var desiredWidth = 2000
+    var desiredWidth = 2048
     pdfjsLib.getDocument(filePath).then(function(pdf) {
-
+      
       pdf.getPage(1).then(function(page) {
 
         var rawViewport = page.getViewport(1)
@@ -39,10 +44,36 @@ export default {
 </script>
 
 <style scoped>
+.pdf-tools {
+  background-color: #222;
+  border-radius: 15px 15px 0 0;
+  padding: 0.5em 1em;
+}
+.pdf-tools-span {
+  margin: 0 1em;
+}
+.pdf-tools-span:first-child {
+  margin: 0;
+}
+.pdf-download-button {
+  color: white;
+}
+.pdf-download-button:hover {
+  color: #ddd;
+  text-decoration: underline;
+}
 .pdf-container {
+  /* border: 1px solid red; */
+  box-shadow: 0px 10px 50px 10px #eee;
+  border-radius: 15px;
   max-width: 100%;
 }
 .pdf-canvas {
+  /* border: 1px dashed grey; */
+  margin: 10px 0;
   max-width: 100%;
+}
+.pdf-instruction {
+  padding: 1em;
 }
 </style>
