8f5dc29b71bd34694d1b5e893127b8f3cf393f5b
Zilong Liang <zlliang15@fudan.edu.cn>
2018-07-20 11:25:07 +0800

    Update iOS support

--- website/.vuepress/theme/components/ArchiveItem.vue
+++ website/.vuepress/theme/components/ArchiveItem.vue
@@ -50,7 +50,7 @@ export default {
 <style scoped>
 .archive-item {
   -webkit-touch-callout: none;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  
   border-radius: 15px;
   margin: 2em 0em !important;
   box-shadow: 0px 5px 20px 5px #e5e5e5;
