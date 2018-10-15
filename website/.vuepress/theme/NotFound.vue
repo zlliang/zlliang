8bb4081f4d34f58f6e7817a152fed3e2cf559246
Zilong Liang <zlliang15@fudan.edu.cn>
2018-10-15 19:30:21 +0800

    Update site

--- website/.vuepress/theme/NotFound.vue
+++ website/.vuepress/theme/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section class="hero is-fullheight">
+    <section class="hero is-medium">
     <Header/>
     <div class="hero-body">
       <div class="container has-text-centered">
