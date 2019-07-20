47b2550375db5aa6ab1f553a8b1d6c6cb62bfc78
Zilong Liang <zlliang96@outlook.com>
2019-07-20 15:53:12 +0800

    Start using TypeScript

---
+++ layouts/page.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import Head from 'next/head'
+
+interface PageProps {
+  children: ReactNode
+}
+
+function Page(props: PageProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+        <link href='https://unpkg.com/modern-normalize' rel='stylesheet' />
+        <title>Zilong Liang</title>
+      </Head>
+      <div className='page'>{props.children}</div>
+    </>
+  )
+}
+
+export default Page
