bf036fa281318e2dfdbaf2847ad2d72a9d4d6dee
Zilong Liang <zlliang96@outlook.com>
2019-07-20 17:00:34 +0800

    Update website

--- layouts/page.tsx
+++ layouts/page.tsx
@@ -1,9 +1,14 @@
 import { ReactNode, ReactElement } from 'react'
 import Head from 'next/head'
 
+import Heading from '../components/heading'
+
 interface PageProps {
+  title?: string
   children: ReactNode
 }
+
+const siteTitle = 'Zilong Liang'
 
 function Page(props: PageProps): ReactElement {
   return (
@@ -13,9 +18,22 @@ function Page(props: PageProps): ReactElement {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
         <link href='https://unpkg.com/modern-normalize' rel='stylesheet' />
-        <title>Zilong Liang</title>
+        <title>
+          {props.title ? `${props.title} | ${siteTitle}` : siteTitle}
+        </title>
       </Head>
-      <div className='page'>{props.children}</div>
+      <div className='page-container'>
+        <Heading />
+        <div className='page'>{props.children}</div>
+      </div>
+      <style jsx>{`
+        .page-container {
+          max-width: 50rem;
+          margin: 0 auto;
+          padding: 0 1rem;
+          color: #2c2c2c;
+        }
+      `}</style>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,39 @@
-import { ReactElement } from 'react'
-
 import Page from '../layouts/page'
 
-export default function Index(): ReactElement {
-  return <Page>Hello World!</Page>
+function Index() {
+  return (
+    <Page>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, odit
+        eaque. Id velit, error nam molestias cum ipsum amet obcaecati voluptas,
+        illum aperiam expedita unde dolor quae adipisci odio incidunt.
+      </p>
+      <p>
+        Quos at quod repellendus sit labore cupiditate harum tempore deserunt
+        eum iusto unde officia quam laboriosam ratione voluptatibus, fuga illo
+        commodi suscipit quisquam qui quibusdam minima ipsum. Nostrum, officiis
+        totam.
+      </p>
+      <p>
+        Iure quasi officiis rerum, at veniam eum perferendis, et impedit quis
+        eius rem enim minus, debitis facere. Quasi aperiam saepe sunt
+        blanditiis, necessitatibus quis rerum accusamus qui dolorem, voluptas
+        dolorum.
+      </p>
+      <p>
+        Odio aperiam enim voluptatibus quam, facere dignissimos numquam sit
+        optio molestias repudiandae, ut accusantium ipsum quod earum dolores
+        adipisci, deserunt expedita quisquam inventore eaque corporis iste a
+        doloribus. Quia, deserunt.
+      </p>
+      <p>
+        Ipsa quia dolor dolorum quidem autem voluptatem eveniet cumque corrupti,
+        architecto asperiores pariatur, iure unde debitis deserunt ea expedita!
+        Esse aspernatur doloremque rerum ea eius provident numquam tenetur
+        excepturi voluptatibus!
+      </p>
+    </Page>
+  )
 }
+
+export default Index
