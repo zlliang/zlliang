29e05c02d126b15bd016f780a7c4b310cf74b610
Zilong Liang <zlliang96@outlook.com>
2019-07-23 21:56:49 +0800

    Add posts

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,20 @@
 import { NextPage } from 'next'
 
 import Page from '../layouts/page'
-import Title from '../components/post/title'
+import Title from '../components/title'
 import Markdown from '../components/markdown'
 
 import content from '../markdown/about.md'
 
 const meta = {
   title: 'About Me',
-  updated: '2019-07-23',
+  created: '2019-07-23',
   github: 'markdown/about.md'
 }
 
 const About: NextPage = () => (
   <Page>
-    <Title meta={meta} />
+    <Title {...meta} />
     <Markdown content={content} />
   </Page>
 )
