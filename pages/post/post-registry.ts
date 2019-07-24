9c349d299cb51c4c0d25ac1cc2d409f431438721
Zilong Liang <zlliang96@outlook.com>
2019-07-24 16:41:22 +0800

    Fix post registry

--- pages/post/post-registry.ts
+++ pages/post/post-registry.ts
@@ -1,13 +1,14 @@
 // Registry of posts
 
-import { meta as MetaRedesignMyWebsite } from './redesign-my-website.md'
-import { meta as MetaPdfjsTutorial } from './pdfjs-tutorial.md'
-import { meta as MetaNaturalButtonsForWeb } from './natural-buttons-for-web.md'
+import { meta as RedesignMyWebsite } from './redesign-my-website.md'
+import { meta as PdfjsTutorial } from './pdfjs-tutorial.md'
+import { meta as NaturalButtonsForWeb } from './natural-buttons-for-web.md'
 import { meta as BuildAStaticLibraryInC } from './build-a-static-library-in-c.md'
+
 const posts = [
-  MetaRedesignMyWebsite,
-  MetaNaturalButtonsForWeb,
-  MetaPdfjsTutorial,
+  RedesignMyWebsite,
+  NaturalButtonsForWeb,
+  PdfjsTutorial,
   BuildAStaticLibraryInC
 ]
 
