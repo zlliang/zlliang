eba817f7e3233dfb115db8e2408900003c1feaab
Zilong Liang <zlliang96@outlook.com>
2019-07-24 10:19:03 +0800

    Optimize code structure: Move markdown parsing to webpack procedure

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,24 +1,5 @@
 import { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
-import MarkdownIt from 'markdown-it'
-import hljs from 'highlight.js'
-// @ts-ignore: This package does not provide type definitions
-import mdKatex from '@iktakahiro/markdown-it-katex'
-
-const md: MarkdownIt = new MarkdownIt({
-  html: true,
-  linkify: true,
-  highlight: (str, lang) => {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return `<pre class='hljs'><div><code class='hljs-lang'>${lang}</code></div><code>${
-          hljs.highlight(lang, str, true).value
-        }</code></pre>`
-      } catch (_) {}
-    }
-    return `<pre class='hljs'><code>${md.utils.escapeHtml(str)}</code></pre>`
-  }
-}).use(mdKatex)
 
 const MarkdownContainer = styled.div`
   img {
@@ -31,7 +12,6 @@ interface MarkdownProps {
 }
 
 const Markdown: FunctionComponent<MarkdownProps> = ({ content }) => {
-  content = md.render(content)
   return <MarkdownContainer dangerouslySetInnerHTML={{ __html: content }} />
 }
 
