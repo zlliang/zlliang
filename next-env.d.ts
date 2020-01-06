5f96ea6768fc3d03fb393f97b4bb4da716421e60
Zilong Liang <zlliang96@outlook.com>
2020-01-06 10:29:09 +0800

    Create yet another next version of my website

--- next-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-
-declare module '*.md' {
-  export const content: string
-  export const meta: PostMeta
-  const Post: NextPage
-  export default Post
-}
