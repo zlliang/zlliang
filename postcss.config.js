cd941740d2ecc8fb793eabbe106b91226b57f311
Zilong Liang <zlliang96@outlook.com>
2020-01-06 11:27:50 +0800

    Add PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./pages/**/*.js"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 };
