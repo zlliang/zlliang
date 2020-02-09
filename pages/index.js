678bc511ffdf72c37a6923a4d73f719129cb8d02
Zilong Liang <zlliang96@outlook.com>
2020-02-09 18:54:37 +0800

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { Global as GlobalStyle, css } from "@emotion/core";
+
 import Page from "../components/layout";
 import Header from "../components/index/header";
 import Bio from "../components/index/bio";
@@ -5,10 +7,25 @@ import Posts from "../components/index/posts";
 
 import Markdown from "../components/markdown";
 import { content } from "./_index.md";
+import { mediaQuery, imageHost } from "../utils/config";
+
+const globalCSS = css`
+  /* Background */
+  body {
+    background-image: url("${imageHost}/background.svg");
+    background-repeat: no-repeat;
+  }
+  ${mediaQuery.phone} {
+    body {
+      background-size: 120%;
+    }
+  }
+`;
 
 export default function Index() {
   return (
     <Page isIndex>
+      <GlobalStyle styles={globalCSS} />
       <Header />
       <Bio />
       <Posts />
