d01edccadf5f161b1b93393e77475b559d18a028
Zilong Liang <zlliang96@outlook.com>
2021-08-28 23:06:03 +0800

    Delete archive

--- archive/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import 'modern-normalize/modern-normalize.css'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import '../styles/highlight.css'
-
-// eslint-disable-next-line react/prop-types
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
