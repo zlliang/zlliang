76283d65d9687fc1f3036fd65bb8554b14b7e848
Zilong Liang <zlliang96@outlook.com>
2021-08-29 10:31:27 +0800

    feat: Add Haskell highlight support

--- src/utils/renderMarkdown.ts
+++ src/utils/renderMarkdown.ts
@@ -3,12 +3,13 @@ import remarkGfm from "remark-gfm"
 import remarkRehype from "remark-rehype"
 import rehypeHighlight from "rehype-highlight"
 import rehypeStringify from "rehype-stringify"
+import haskell from "highlight.js/lib/languages/haskell"
 
 export function renderMarkdown(markdown: string): string {
   const processor = remark()
     .use(remarkGfm)
     .use(remarkRehype)
-    .use(rehypeHighlight)
+    .use(rehypeHighlight, { languages: { haskell } })
     .use(rehypeStringify)
 
   const result = processor.processSync(markdown)
