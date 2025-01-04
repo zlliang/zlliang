afb46c0ec9cc7c0b856d90c17676c054d325fb6e
Zilong Liang <zlliang96@outlook.com>
2025-01-04 09:00:07 +0800

    content: Migrate some history articles

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,18 +1,18 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.NavItem[] = [
-  // {
-  //   text: '🛠️ 技术',
-  //   activeMatch: '/tech-notes/',
-  //   items: [
-  //     {
-  //       text: '首页 - Tech Notes',
-  //       link: '/tech-notes/',
-  //     },
-  //   ],
-  // },
   {
-    text: '💭 成长',
+    text: '🛠️ Tech Notes',
+    activeMatch: '/tech-notes/',
+    items: [
+      {
+        text: '首页 - Tech Notes',
+        link: '/tech-notes/',
+      },
+    ],
+  },
+  {
+    text: '💭 Evolving Me',
     activeMatch: '/evolving-me/',
     items: [
       {
@@ -26,16 +26,35 @@ export const nav: DefaultTheme.NavItem[] = [
 export const sidebar: DefaultTheme.Sidebar = {
   '/evolving-me/': [
     {
-      text: '主页 - Evolving Me',
+      text: '首页 - Evolving Me',
       link: '/evolving-me/',
     },
     {
       text: '笔记列表',
       items: [
         {
-          text: '迈入 2025',
+          text: '迈入 2025：保持高能量',
           link: '/evolving-me/into-2025',
         },
+        {
+          text: '警惕“Work Life Balance”中的话语陷阱',
+          link: '/evolving-me/the-trap-of-wlb',
+        }
+      ],
+    },
+  ],
+  '/tech-notes/': [
+    {
+      text: '首页 - Tech Notes',
+      link: '/tech-notes/',
+    },
+    {
+      text: '笔记列表',
+      items: [
+        {
+          text: '什么是 Advent of Code',
+          link: '/tech-notes/what-is-aoc',
+        }
       ],
     },
   ],
