5cea71c9e8213ce74f8a9759b99169e29f7b86da
Zilong Liang <zlliang96@outlook.com>
2025-01-04 13:48:15 +0800

    content: Migrate some history articles

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -37,6 +37,10 @@ export const sidebar: DefaultTheme.Sidebar = {
           link: '/evolving-me/into-2025',
         },
         {
+          text: '加入离职赛道',
+          link: '/evolving-me/leaving-tencent',
+        },
+        {
           text: '警惕“Work Life Balance”中的话语陷阱',
           link: '/evolving-me/the-trap-of-wlb',
         }
@@ -51,6 +55,18 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: '笔记列表',
       items: [
+        {
+          text: '如何将一个 JS object 复制为 plain object',
+          link: '/tech-notes/how-to-freeze-an-object-in-js'
+        },
+        {
+          text: 'Shell 脚本中的单引号、引号和反引号',
+          link: '/tech-notes/quotes-in-shell-script',
+        },
+        {
+          text: '通配符（globbing）和正则表达式（regular expression）的异同',
+          link: '/tech-notes/globbing-and-regex',
+        },
         {
           text: '什么是 Advent of Code',
           link: '/tech-notes/what-is-aoc',
