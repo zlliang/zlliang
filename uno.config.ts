4d7c986b8a5682090221a8068f8513597c1d9f14
Zilong Liang <zlliang96@outlook.com>
2025-01-22 15:50:13 +0800

    feat: Adopt Iconify

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig, transformerDirectives } from 'unocss'
+import { defineConfig, presetUno, presetIcons, transformerDirectives } from 'unocss'
 
 export default defineConfig({
+  presets: [
+    presetUno(),
+    presetIcons({
+      collections: {
+        lucide: () => import('@iconify-json/lucide/icons.json').then(i => i.default),
+      },
+      extraProperties: {
+        'display': 'inline-block',
+        'width': '1.2em',
+        'height': '1.2em',
+        'vertical-align': 'text-bottom',
+        'transform': 'scale(0.95)',
+      }
+    }),
+  ],
   transformers: [
     transformerDirectives(),
   ],
