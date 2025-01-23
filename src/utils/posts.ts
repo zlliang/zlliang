acb05bee691902f6fef524bce949ce4b15ef1b60
Zilong Liang <zlliang96@outlook.com>
2025-01-23 17:22:17 +0800

    refactor: Reorganize the posts utilities

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,5 +1,18 @@
 import { getCollection } from 'astro:content'
+import { groupBy } from 'lodash-es'
+import { getYear } from 'date-fns'
 
+/** All posts */
 export async function getPosts() {
   return await getCollection('posts', ({ data }) => import.meta.env.PROD ? data.draft !== true : true)
+}
+
+/** All posts grouped and sorted by created dates */
+export async function getGroupedPosts() {
+  const posts = await getPosts()
+  const groups = Object.entries(groupBy(posts, (post) => getYear(post.data.created)))
+    .map(([year, posts]) => ({ year, posts: posts.toSorted((a, b) => b.data.created.valueOf() - a.data.created.valueOf()) }))
+    .toSorted((a, b) => Number(b.year) - Number(a.year))
+  
+  return groups
 }
