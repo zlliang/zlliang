f9f7ae119825dac809f27bf80fd5b56547e509da
Zilong Liang <zlliang96@outlook.com>
2025-01-23 21:23:36 +0800

    refactor: Update the posts utilities

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -2,16 +2,19 @@ import { getCollection } from 'astro:content'
 import { groupBy } from 'lodash-es'
 import { getYear } from 'date-fns'
 
-/** All posts */
+/** All posts sorted by created dates */
 export async function getPosts() {
-  return await getCollection('posts', ({ data }) => import.meta.env.PROD ? data.draft !== true : true)
+  const collection = await getCollection('posts', ({ data }) => import.meta.env.PROD ? data.draft !== true : true)
+  const posts = collection.toSorted((a, b) => b.data.created.valueOf() - a.data.created.valueOf())
+
+  return posts
 }
 
-/** All posts grouped and sorted by created dates */
+/** All posts grouped by years */
 export async function getGroupedPosts() {
   const posts = await getPosts()
   const groups = Object.entries(groupBy(posts, (post) => getYear(post.data.created)))
-    .map(([year, posts]) => ({ year, posts: posts.toSorted((a, b) => b.data.created.valueOf() - a.data.created.valueOf()) }))
+    .map(([year, posts]) => ({ year, posts }))
     .toSorted((a, b) => Number(b.year) - Number(a.year))
   
   return groups
