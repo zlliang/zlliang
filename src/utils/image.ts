cd392177e46ddd3d3857d4f37f77f24c04062736
Zilong Liang <zlliang96@outlook.com>
2025-02-05 21:24:00 +0800

    perf: Adjust ImageKit URLs

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,6 +1,6 @@
-/** Use [ImageKit](https://imagekit.io/) to optimize images */
+/** Use [ImageKit](https://imagekit.io/) to optimize external images */
 export function getOptimizedImageUrl(rawImageUrl?: string) {
   if (!rawImageUrl) return
   
-  return `https://ik.imagekit.io/zlliang/tr:w-1024/${encodeURIComponent(rawImageUrl)}`
+  return `https://ik.imagekit.io/zlliang/tr:w-1280,h-1280,c-at_max/${encodeURIComponent(rawImageUrl)}`
 }
