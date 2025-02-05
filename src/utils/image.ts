0279cdc9b8c39a75bf2dbabee3bd452301897363
Zilong Liang <zlliang96@outlook.com>
2025-02-06 07:21:52 +0800

    fix: Rearrange the ISR to time-based invalidation

--- src/utils/image.ts
+++
@@ -1,6 +0,0 @@
-/** Use [ImageKit](https://imagekit.io/) to optimize external images */
-export function getOptimizedImageUrl(rawImageUrl?: string) {
-  if (!rawImageUrl) return
-  
-  return `https://ik.imagekit.io/zlliang/tr:w-1280,h-1280,c-at_max/${encodeURIComponent(rawImageUrl)}`
-}
