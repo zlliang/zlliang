2d101b59620764e7e402e0ae3642e7f931262ac6
Zilong Liang <zlliang96@outlook.com>
2025-02-07 23:26:47 +0800

    fix: Fix the image URL retrieving method

--- src/utils/meals.ts
+++ src/utils/meals.ts
@@ -15,7 +15,7 @@ export async function getMeals() {
     date: (properties['日期'].date?.start as string | undefined) ? new Date(properties['日期'].date.start) : undefined,
     type: properties['类型'].select?.name as string | undefined,
     source: properties['来源'].select?.name as string | undefined,
-    image: getOptimizedImageUrl(properties['照片'].files[0]?.file.url as string | undefined),
+    image: getOptimizedImageUrl((properties['照片'].files[0]?.file?.url || properties['照片'].files[0]?.external?.url) as string | undefined),
     price: properties['价格'].number as number | undefined,
     description: properties['备注'].rich_text[0]?.text.content as string | undefined,
   }))
