28810200c8884f26a588b1e359e18f569b2204f4
Zilong Liang <zlliang96@outlook.com>
2025-03-30 08:37:29 +0800

    refactor: Rearrange the structure of content collections

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,8 +2,8 @@ import { format as defaultFormat } from 'date-fns'
 import { zhCN } from 'date-fns/locale'
 import { TZDate } from '@date-fns/tz'
 
+export const Date = TZDate
+
 export const format: typeof defaultFormat = (date, formatStr, options) => {
   return defaultFormat(date, formatStr, { locale: options?.locale || zhCN })
 }
-
-export const Date = TZDate
