b5dadf3cafdc8173e56d39f7e1c871066e3d2d2b
Zilong Liang <zlliang96@outlook.com>
2025-11-02 18:58:05 +0800

    feat: simplify the whole site and remove ESLint

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,18 +1,18 @@
-import type { CollectionEntry } from 'astro:content'
-import { getCollection, getEntry, render } from 'astro:content'
-import { getYear } from 'date-fns'
-import { groupBy, uniq } from 'lodash-es'
+import type { CollectionEntry } from "astro:content"
+import { getCollection, getEntry, render } from "astro:content"
+import { getYear } from "date-fns"
+import { groupBy, uniq } from "lodash-es"
 
 /** All posts sorted by created dates */
 export async function getPosts() {
-  const collection = await getCollection('posts', ({ data }) => import.meta.env.PROD ? !data.draft : true)
+  const collection = await getCollection("posts", ({ data }) => import.meta.env.PROD ? !data.draft : true)
   const posts = collection.toSorted((a, b) => b.data.created.valueOf() - a.data.created.valueOf())
 
   return posts
 }
 
 /** Group posts by years */
-export function groupByYear(posts: CollectionEntry<'posts'>[]) {
+export function groupByYear(posts: CollectionEntry<"posts">[]) {
   const groups = Object.entries(groupBy(posts, post => getYear(post.data.created)))
     .map(([year, posts]) => ({ year, posts }))
     .toSorted((a, b) => Number(b.year) - Number(a.year))
@@ -21,7 +21,7 @@ export function groupByYear(posts: CollectionEntry<'posts'>[]) {
 }
 
 /** Get tags from posts */
-export function getTags(posts: CollectionEntry<'posts'>[]) {
+export function getTags(posts: CollectionEntry<"posts">[]) {
   const tags = uniq(posts.flatMap(post => post.data.tags).filter(Boolean)) as string[]
 
   return tags
@@ -29,7 +29,7 @@ export function getTags(posts: CollectionEntry<'posts'>[]) {
 
 /** Render a fragment */
 export async function renderFragment(slug: string) {
-  const entry = await getEntry('fragments', slug)!
+  const entry = await getEntry("fragments", slug)!
   const { Content } = await render(entry)
 
   return Content
--- src/utils/rehype.ts
+++ src/utils/rehype.ts
@@ -1,17 +1,17 @@
-import type { Root } from 'hast'
+import type { Root } from "hast"
 
-import { visit } from 'unist-util-visit'
+import { visit } from "unist-util-visit"
 
 /** Retrieve the `title` property of the `img` element and append a caption element after it. */
 export function rehypeImageCaption() {
   return (tree: Root) => {
-    visit(tree, 'element', (node, index, parent) => {
-      if (parent && node.tagName === 'img' && node.properties.title) {
+    visit(tree, "element", (node, index, parent) => {
+      if (parent && node.tagName === "img" && node.properties.title) {
         parent.children.push({
-          type: 'element',
-          tagName: 'span',
-          properties: { class: 'caption' },
-          children: [{ type: 'text', value: node.properties.title as string }],
+          type: "element",
+          tagName: "span",
+          properties: { class: "caption" },
+          children: [{ type: "text", value: node.properties.title as string }],
         })
       }
     })
