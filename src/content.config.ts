5e708644da33d4fd50ac93025e060135bc36369b
Zilong Liang <zlliang96@outlook.com>
2025-11-03 09:22:08 +0800

    feat: adjust styles

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { glob } from "astro/loaders"
 import { defineCollection, z } from "astro:content"
+import { glob } from "astro/loaders"
 
 const posts = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
