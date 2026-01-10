cd8f7ab1b7963b09441748e0f3326632a00fd4ec
Zilong Liang <zlliang96@outlook.com>
2026-01-10 11:31:34 +0800

    chore: update dependencies and site metadata, refs #76

--- websites/tech/src/utils/site.ts
+++ websites/tech/src/utils/site.ts
@@ -1,4 +1,4 @@
-export const siteTitle = "Zilong Liang"
+export const siteTitle = "Zilong's Tech Blog"
 
 export function getTitle(title?: string) {
   return title ? `${title} - ${siteTitle}` : siteTitle
