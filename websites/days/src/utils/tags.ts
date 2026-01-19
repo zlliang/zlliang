496912c2150d68d2492eec02a18e1315d4e7722a
Zilong Liang <zlliang96@outlook.com>
2026-01-19 17:19:48 +0800

    docs(days): add a new note

--- websites/days/src/utils/tags.ts
+++ websites/days/src/utils/tags.ts
@@ -6,7 +6,9 @@ export function getTagDisplay(tag: string) {
 const registry: Record<string, string> = {
   "coffee": "咖啡",
   "demo": "示例",
+  "festivals": "节日",
   "gaming": "游戏",
+  "home": "居家",
   "japanese-drama": "日剧",
   "watching": "观影",
   "writing": "写作",
