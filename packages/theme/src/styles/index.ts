28c12cef304e878b6ec4c6ba7b97c95b39e5a1fa
Zilong Liang <zlliang96@outlook.com>
2026-09-13 22:36:53 +0800

    build(deps): replace clsx and tailwind-merge with cn

--- packages/theme/src/styles/index.ts
+++
@@ -1,9 +0,0 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-import type { ClassValue } from "clsx"
-
-/** Composes classnames */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
